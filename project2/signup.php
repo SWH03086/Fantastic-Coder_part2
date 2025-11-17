d8af6cf99330fbde41af76fa647f3592ea754080
SWH03089 <106208101@student.swin.edu.au>
2025-11-17 14:17:34 +0700

    minor change

--- project2/signup.php
+++ project2/signup.php
@@ -33,7 +33,7 @@ if ($result) {
           <label for="username">Username:</label>
     <input type="text" name="username" required><br>
     <label for="password">Password:</label>
-    <input type="password" name="password" required><br>
+    <input type="password" name="password" required><br><br>
     <input type="hidden" name="token" value="abc123">
     <input type="submit" value="Sign up" class="btn-submit">
         </div>
